9d25f1f75f1f5918657bf343986cf7ccb372fe2c
Matías Sánchez Abrego <matiassanchezabrego@gmail.com>
2022-01-23 23:50:44 -0300

    Commits de models/views

--- AppEntrega1/models.py
+++ AppEntrega1/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Seccion(models.Model):
+    
+    nombre=models.CharField(max_length=40, verbose_name='Descripcion de la Sección')
+    piso = models.IntegerField(verbose_name='Piso de la biblioteca en que está ubicada la sección')
+    
+    def __str__(self):
+        return f'Sección de {self.nombre}, ubicada en el piso ({self.piso})'
+    
+class Libro(models.Model):
+    nombre = models.CharField(max_length=30, verbose_name='Nombre completo del libro')
+    autor = models.CharField(max_length=50)
+    fecha_publicación = models.DateField()
+    editorial = models.TextField()
+    disponibilidad = models.BooleanField()
+    sitio_oficial = models.URLField()
+    
+    def __str__(self):
+        return f'Nombre del libro: {self.nombre}, escrito por {self.autor} el {self.fecha_publicación}'
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido} ({self.email})'
--- AppEntrega1/views.py
+++ AppEntrega1/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from django.http import HttpResponse
+
+from .models import Seccion, Libro, Cliente
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
+
+def inicio(request):
+    return render(request, 'AppEntrega1/inicio.html')
+
+def seccion(request):
+    return render(request, 'AppEntrega1/seccion.html', {'seccion': Seccion.objects.all()})
+
+def libro(request):
+    return HttpResponse('libro')
+
+def cliente(request):
+    return HttpResponse('cliente')
+class SeccionListView(ListView):
+    model = Seccion
+    template_name = 'AppEntrega1/seccion.html'
+    context_object_name = 'seccion'
+class SeccionCreateView(CreateView):
+    model = Seccion
+    success_url = reverse_lazy('seccion')
+    fields = ['nombre', 'piso']
+    # template_name = 'AppEntrega1/profesor_form.html'
